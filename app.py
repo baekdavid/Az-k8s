e5f316ef16cc3857a8e580b348497908530ca137
Chinseok Lee <me@askcompany.kr>
2019-06-11 14:42:58 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import platform
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"I'm in {platform.node()}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
+
